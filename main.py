cf9721c24312987c9eccdf7a399c15f893983c2e
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2020-11-27 16:32:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,14 +16,11 @@ print(value_1)
 def function_2(x1, x2): # функция 2
     return x1**2*np.cos(x2) + 0.05*(x2)**3 + 3*(x1)**3*np.log2(x2**2)
 
-def gradient(list_of_values, function): # задание 2
-    list_lims = []    
+def gradient(list_of_values, function): # задание 2     
     delta = 0.00001      
-    lim_x = (function(x1 + delta, x2) - function(x1, x2)) / delta
-    list_lims.append(round(lim_x, 2))
-    lim_y = (function(x1, x2 + delta) - function(x1, x2)) / delta
-    list_lims.append(round(lim_y, 2))
-    return list_lims
+    lim_x = (function(x1 + delta, x2) - function(x1, x2)) / delta    
+    lim_y = (function(x1, x2 + delta) - function(x1, x2)) / delta    
+    return [round(lim_x, 2), round(lim_y, 2)]
 
 x1 = 10
 x2 = 1
