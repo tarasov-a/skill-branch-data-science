78f5cfa1909247a3df702e27bf414b6e14030352
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2020-11-28 08:54:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -45,16 +45,16 @@ print(value_3)
 
 
 
-def gradient_optimization_multi_dim(function): # задание 4   
+def gradient_optimization_multi_dim(function): # задание 3   
     delta = 0.00001
-    x1, x2 = 4, 10   
-    N = 50
-    e = 0.001 
+    x1, x2 = 4, 10   # начальные позиции
+    N = 50 # количество итераций
+    e = 0.001 # шаг
     for i in range(N):
         lim_x = (function([x1 + delta, x2]) - function([x1, x2])) / delta
         lim_y = (function([x1, x2 + delta]) - function([x1, x2])) / delta 
-        x1 = x1 - e*lim_x
-        x2 = x2 - e*lim_y
+        x1 = x1 - round(e*lim_x, 2)
+        x2 = x2 - round(e*lim_y, 2)
     return [round(x1, 2), round(x2, 2)]  
     
 value_4 = gradient_optimization_multi_dim(function_2)
