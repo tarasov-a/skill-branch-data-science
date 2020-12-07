e0c07e325c09d7180d0bca73afdd549db5f494cc
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2020-12-07 19:04:48 +0300

    Update task_2_solution.py

--- task_2_solution.py
+++ task_2_solution.py
@@ -22,3 +22,9 @@ def calculate_squad_in_cheap_apartment(x): # Задание 6.
     price = 1000000
     cheap_ap = x[x['cost'] < price]
     return round(np.mean(cheap_ap['full_sq']))
+
+def calculate_mean_price_in_new_housing(x): # Задание 7.
+    rooms = 3
+    year = 2010
+    new_ap = x[(x['rooms'] == rooms) & (apartments['year'] >= year)]
+    return round(np.mean(new_ap['cost']))
