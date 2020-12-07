19fd8343234cd8fb0d40b214e0ff011b3ffdb75c
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2020-12-07 17:35:09 +0300

    Update task_2_solution.py

--- task_2_solution.py
+++ task_2_solution.py
@@ -5,7 +5,9 @@ def take_columns(x): # Задание 2.
     return x.columns
 
 def calculate_target_ratio(x, target_name): # Задание 3.
-    return np.mean(x[target_name])
+    return round(np.mean(x[target_name]), 2)
 
 def calculate_data_dtypes(x): # Задание 4.
     return x.dtypes.value_counts()
+
+
