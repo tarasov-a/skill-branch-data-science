ba36ff9ec9d214d45be0a2e9db8462b3928157f2
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2020-12-07 18:30:34 +0300

    Update task_2_solution.py

--- task_2_solution.py
+++ task_2_solution.py
@@ -10,4 +10,8 @@ def calculate_target_ratio(x, target_name): # Задание 3.
 def calculate_data_dtypes(x): # Задание 4.
     return x.dtypes.value_counts()
 
+def calculate_cheap_apartment(x): # Задание 5.
+    price = 1000000
+    return len(apartments[apartments['cost'] < price])
+
 
