c7adc93f903e1cab76dbcac6c44eaec05392e93e
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2021-02-23 10:12:04 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -1,1 +1,12 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
 
+
+def split_data_into_two_samples(x): # Задание 1.
+    x_train, x_test = train_test_split(x, test_size=0.3, shuffle=True, random_state=42)
+    return x_train, x_test
