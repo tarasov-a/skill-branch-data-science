ddc09f9eebdf5889b36d73d565ab3d81fd8a9c6d
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2021-02-24 10:04:28 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -11,11 +11,15 @@ def split_data_into_two_samples(x): # Задание 1.
     x_train, x_test = train_test_split(x, test_size=0.3, shuffle=True, random_state=42)
     return x_train, x_test
 
-
-def prepare_data(x):
-    price_doc = x['SalePrice']
-    objects = x.select_dtypes(['object'])
-    data_x = x.drop(objects, axis=1).drop(['Id'], axis=1).dropna(axis=1)
-    return data_x, price_doc
+def scale_data(x, type_of_scaler): # Задание 3
+    numeric_data_features = x.select_dtypes([np.number]).columns
+    if type_of_scaler == MinMaxScaler:        
+        scaler = MinMaxScaler()
+        x_scaled = scaler.fit_transform(x[numeric_data_features])
+        return x_scaled
+    if type_of_scaler == StandardScaler:
+        scaler = StandardScaler()
+        x_scaled = scaler.fit_transform(x[numeric_data_features])
+        return x_scaled
 
 
