4f088102614672fddab540745f689c1c208d60c4
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2021-02-25 11:23:13 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -17,30 +17,23 @@ def prepare_data(df): # Задание 2.
     data_x = df.drop(objects, axis=1).drop(['id', 'price_doc'], axis=1).dropna(axis=1)
     return data_x, price_doc
 
-def scale_data(df, transformer): # Задание 3
-    numeric_data_features = df.select_dtypes([np.number]).columns
-    if transformer == MinMaxScaler:        
-        scaler = MinMaxScaler()
-        x_scaled = scaler.fit_transform(df[numeric_data_features])
-        return pd.DataFrame(x_scaled)
-    if transformer == StandardScaler:
-        scaler = StandardScaler()
-        x_scaled = scaler.fit_transform(df[numeric_data_features])
-        return pd.DataFrame(x_scaled)
+def scale_data(df, transformer): # Задание 3.
+    numeric_data_features = df.select_dtypes([np.number]).columns    
+    x_scaled = transformer.fit_transform(df[numeric_data_features])
+    return pd.DataFrame(x_scaled)    
     
-    
-def prepare_data_for_model(x, transformer): # Задание 4.
-    price_doc = x['price_doc']
-    objects = x.select_dtypes(['object'])
-    data_x = x.drop(objects, axis=1).drop(['id', 'price_doc'], axis=1).dropna(axis=1)
-    if transformer == MinMaxScaler:
-        scaler = MinMaxScaler()
-        x_train_scaled = scaler.fit_transform(data_x)
-        return x_train_scaled, price_doc
-    if transformer == StandardScaler:
-        scaler = StandardScaler()
-        x_train_scaled = scaler.fit_transform(data_x)
-        return x_train_scaled, price_doc
+def prepare_data_for_model(df, transformer): # Задание 4.
+    price_doc = df['price_doc']
+    objects = df.select_dtypes(['object'])
+    data_x = df.drop(objects, axis=1).drop(['id', 'price_doc'], axis=1).dropna(axis=1)
+    x_scaled = transformer.fit_transform(data_x)     
+    return pd.DataFrame(x_scaled), price_doc
+
+def fit_first_linear_model(x_train, y_train): # Задание 5.
+    model = LinearRegression()
+    model.fit(x_train, y_train)    
+    y_train_pred = model.predict(x_train)    
+    return y_train_pred 
  
 
 
