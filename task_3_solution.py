2a3fda079d5f4743519e7e4d8c1a46921dbff53e
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2021-02-26 11:19:12 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -9,4 +9,4 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 def split_data_into_two_samples(df): # Задание 1.  
     x_train, x_valid = train_test_split(df, train_size=0.7, shuffle=True, random_state=42)
     y_train, y_valid = train_test_split(df['price_doc'], train_size=0.7, shuffle=True, random_state=42)
-    return x_train, y_train, x_valid, y_valid
+    return x_train, x_valid, y_train, y_valid
