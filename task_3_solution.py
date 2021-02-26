db6371f4c2a2115f3e2e8ab30fb82433abe1e093
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2021-02-26 15:00:53 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -14,8 +14,9 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
-def split_data_into_two_samples(df):
-    return train_test_split(df,test_size = 0.3, train_size = 0.7, shuffle=True, random_state = 42)
+def split_data_into_two_samples(df): 
+    x_train, x_valid = train_test_split(df, train_size=0.7, shuffle=True, random_state=42)
+    return x_train, x_valid
     
 def prepare_data(df_main):
     df = df_main.copy()
@@ -38,7 +39,7 @@ def prepare_data_for_model(df_main, scaler = StandardScaler()):
     return [scale_data(df, scaler), g_var]
     
 def fit_first_linear_model(x_train, y_train):
-    [x_train, y_train, features] = prepare_data_for_model(d_train)
+    [x_train, y_train, features] = prepare_data_for_model(y_train)
     model = LinearRegression()
     model.fit(x_train, y_train)
     return model
@@ -51,10 +52,10 @@ def fit_first_linear_model_cop(x_train, y_train):
     
 def evaluate_model(model, x_test, y_test):
     y_pred = model.predict(x_test)
-    mse = round(mean_squared_error(y_test, y_pred), 2)
-    mae = round(mean_absolute_error(y_test, y_pred), 2)
-    r2 = round(r2_score(y_test, y_pred), 2)
-    return [mse, mae, r2]
+    MSE = round(mean_squared_error(y_test, y_pred), 2)
+    MAE = round(mean_absolute_error(y_test, y_pred), 2)
+    R2 = round(r2_score(y_test, y_pred), 2)
+    return [MSE, MAE, R2]
     
 def calculate_model_weights(model, features):
     sorted_weights = sorted(zip(model.coef_, features), reverse=True)
