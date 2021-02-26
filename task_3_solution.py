e8683c55e96b7f583d6529fb2ce0676b4198cff9
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2021-02-26 15:26:36 +0300

    Update task_3_solution.py

--- task_3_solution.py
+++ task_3_solution.py
@@ -13,42 +13,6 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
-def split_data_into_two_samples(df): 
-    x_train, x_valid = train_test_split(df, train_size=0.7, shuffle=True, random_state=42)
-    return x_train, x_valid
-    
-def prepare_data(df_main):
-    df = df_main.copy()
-    for x in df.columns:
-        if df[x].dtype == 'object':
-            df.drop(x, axis = 1, inplace = True)
-    df.drop('id', axis = 1, inplace = True)
-    df.dropna(axis = 1, inplace = True)
-    g_var = df['price_doc']
-    df.drop('price_doc', axis = 1, inplace = True)
-    return [df, g_var]
-    
-def scale_data(df_main, scaler = StandardScaler()):
-    df = df_main
-    scaler.fit(df)
-    return scaler.transform(df)
-    
-def prepare_data_for_model(df_main, scaler = StandardScaler()):
-    [df, g_var] = prepare_data(df_main)
-    return [scale_data(df, scaler), g_var]
-    
-def fit_first_linear_model(x_train, y_train):
-    [x_train, y_train, features] = prepare_data_for_model(y_train)
-    model = LinearRegression()
-    model.fit(x_train, y_train)
-    return model
-
-def fit_first_linear_model_cop(x_train, y_train):
-    [x_train, y_train, features] = prepare_data_for_model(d_train, MinMaxScaler())
-    model = LinearRegression()
-    model.fit(x_train, y_train)
-    return model
     
 def evaluate_model(model, x_test, y_test):
     y_pred = model.predict(x_test)
@@ -56,10 +20,4 @@ def evaluate_model(model, x_test, y_test):
     MAE = round(mean_absolute_error(y_test, y_pred), 2)
     R2 = round(r2_score(y_test, y_pred), 2)
     return [MSE, MAE, R2]
-    
-def calculate_model_weights(model, features):
-    sorted_weights = sorted(zip(model.coef_, features), reverse=True)
-    weights = pd.Series([x[0] for x in sorted_weights])
-    features = pd.Series([x[1] for x in sorted_weights])
-    df = pd.DataFrame({'features': features, 'weights': weights})
-    return df
+
