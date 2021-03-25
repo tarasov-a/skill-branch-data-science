50d8b7d57136e3d59bb59dd15a8860078a70729e
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2021-03-25 15:56:16 +0300

    Update task_5_solution.py

--- task_5_solution.py
+++ task_5_solution.py
@@ -1,1 +1,18 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
 
+def prepare_data(df): # Задание 1.
+    target = df['isFraud']
+    df_dropped = df.drop(['isFraud', 'TransactionID', 'TransactionDT'], axis=1)
+    return df_dropped, target
+  
+  def fit_first_model(x, y, x_test, y_test): # Задание 2.  
+    x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.7, random_state=1)
+    tree = DecisionTreeClassifier(random_state=1)
+    tree.fit(x_train, y_train)
+    valid_pred = tree.predict_proba(x_valid)
+    score = roc_auc_score(y_valid, valid_pred[:,1])
+    return round(score, 4)
