e815fe4536c1868a4e7b09cc8237e8cf4c6edfb1
tarasov-a <72355884+tarasov-a@users.noreply.github.com>
2021-03-26 11:00:49 +0300

    Update task_5_solution.py

--- task_5_solution.py
+++ task_5_solution.py
@@ -4,12 +4,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import roc_auc_score
 
+
 def prepare_data(df): # Задание 1.
     target = df['isFraud']
     df_dropped = df.drop(['isFraud', 'TransactionID', 'TransactionDT'], axis=1)
     return df_dropped, target
-  
-  def fit_first_model(x, y, x_test, y_test): # Задание 2.  
+
+def fit_first_model(x, y, x_test, y_test): # Задание 2. 
+    x = x.fillna(0)
+    x_test = x_test.fillna(0)
+    x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.7, random_state=1)
+    tree = DecisionTreeClassifier(random_state=1)
+    tree.fit(x_train, y_train)
+    valid_pred = tree.predict_proba(x_valid)
+    score = roc_auc_score(y_valid, valid_pred[:,1])
+    return round(score, 4) 
+
+def fit_second_model(x, y, x_test, y_test): # Задание 3.
+    x = x.fillna(-9999)
+    x_test = x_test.fillna(-9999)
     x_train, x_valid, y_train, y_valid = train_test_split(x, y, train_size=0.7, random_state=1)
     tree = DecisionTreeClassifier(random_state=1)
     tree.fit(x_train, y_train)
